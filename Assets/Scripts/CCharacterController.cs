c494c0f3b50011823bdb0d015baf51767bc5a9a7
lacey0905 <lacey0905@naver.com>
2019-04-23 18:34:47 +0900

    스프라이트

---
+++ Assets/Scripts/CCharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCharacterController : MonoBehaviour
+{
+
+    public float m_fSpeed = 10f;
+    Rigidbody Rigidbody;
+    Vector3 movement;
+
+
+    private void Awake() {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        movement.Set(h, 0, 0);
+        movement = movement.normalized * m_fSpeed * Time.deltaTime;
+
+        Rigidbody.MovePosition(transform.position + movement);
+    }
+
+
+}
