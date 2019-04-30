b67f19731930b992f2244b2c2f14d1d3b08519c4
lacey0905 <lacey0905@naver.com>
2019-04-30 16:47:02 +0900

    플랫포머 강좌

---
+++ Assets/Scripts/CPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (CController2D))]
+public class CPlayer : MonoBehaviour
+{
+    float moveSpeed = 6f;
+    float gravity = -20f;
+    Vector3 velocity;
+
+    CController2D controller;
+
+
+    void Awake() {
+        controller = GetComponent<CController2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
